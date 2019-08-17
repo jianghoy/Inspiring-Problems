aeec77554b9bea386f554fa8e071461b9fe2a191
Jianghong Ying <YJianghong@gmail.com>
2019-08-17 03:28:57 -0400

    add error message

--- src/TestTreeCheck.java
+++ src/TestTreeCheck.java
@@ -1,7 +1,6 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class TestTreeCheck {
 
@@ -9,44 +8,50 @@ public class TestTreeCheck {
     public void testRightCase() {
         TreeCheck tc = new TreeCheck();
         int[][] relationOne = new int[][]{{0,1}};
-        assertTrue(tc.treeCheck(relationOne,2));
+        assertEquals("S",tc.treeCheck(relationOne,2));
         tc.treeCheck(null,1);
         int[][] relationTwo = new int[][]{{0,1},{0,2}};
-        assertTrue(tc.treeCheck(relationTwo,3));
+        assertEquals("S",tc.treeCheck(relationTwo,3));
         int[][] relationThree = new int[][]{{0,1},{1,2}};
-        assertTrue(tc.treeCheck(relationThree,3));
+        assertEquals("S",tc.treeCheck(relationThree,3));
         int[][] relationFour = new int[][]{{0,1},{1,2},{0,3}};
-        assertTrue(tc.treeCheck(relationFour,4));
+        assertEquals("S",tc.treeCheck(relationFour,4));
     }
     @Test
     public void testCycle() {
         TreeCheck tc = new TreeCheck();
         int[][] relation = new int[][]{{0,1},{1,2},{2,0}};
-        assertFalse(tc.treeCheck(relation,3));
-        relation = new int[][]{{0,0}};
-        assertFalse(tc.treeCheck(relation,1));
+        assertEquals("E3",tc.treeCheck(relation,3));
+
+    }
+
+    @Test
+    public void testSelfCycling() {
+        TreeCheck tc = new TreeCheck();
+        int[][] relation = new int[][]{{0,0}};
+        assertEquals("E5",tc.treeCheck(relation,1));
         relation = new int[][]{{0,0},{1,1},{2,2}};
-        assertFalse(tc.treeCheck(relation,3));
+        assertEquals("E5",tc.treeCheck(relation,3));
     }
 
     @Test
     public void testForest() {
         TreeCheck tc = new TreeCheck();
         int[][] relation = new int[][]{{0,1},{2,3}};
-        assertFalse(tc.treeCheck(relation,4));
+        assertEquals("E4",tc.treeCheck(relation,4));
     }
 
     @Test
     public void testMultiChildren() {
         TreeCheck tc = new TreeCheck();
         int[][] relation = new int[][]{{0,1},{0,2},{0,3}};
-        assertFalse(tc.treeCheck(relation,4));
+        assertEquals("E1",tc.treeCheck(relation,4));
     }
 
     @Test
     public void testMultiParent() {
         TreeCheck tc = new TreeCheck();
         int[][] relation = new int[][]{{0,1},{2,1}};
-        assertFalse(tc.treeCheck(relation,3));
+        assertEquals("E3",tc.treeCheck(relation,3));
     }
 }
--- src/TreeCheck.java
+++ src/TreeCheck.java
@@ -3,13 +3,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class TreeCheck {
-    public boolean treeCheck(int[][] relationships, int n) {
+    public String treeCheck(int[][] relationships, int n) {
         if (n < 0) {
-            return false;
+            return "E5";
         }
         // for no node or one node return true
         if ((relationships == null || relationships.length == 0) && (n == 0 || n == 1)) {
-            return true;
+            return "S";
         }
         int[] ancestor = new int[n];
         Arrays.fill(ancestor, -1);
@@ -17,12 +17,21 @@ public class TreeCheck {
         Map<Integer, TreeNode> nodeMap = new HashMap<>();
 
         for (int[] relation : relationships) {
+            if (relation[0] == relation[1]) {
+                return "E5";
+            }
             TreeNode parent = nodeMap.getOrDefault(relation[0], new TreeNode(relation[0]));
             TreeNode child = nodeMap.getOrDefault(relation[1], new TreeNode(relation[1]));
 
             // local relationship building
+
+
+
             if (parent.left != null && parent.right != null) {
-                return false;
+                if (parent.left.val == child.val || parent.right.val == child.val) {
+                    return "E2";
+                }
+                return "E1";
             }
             if (parent.right == null) {
                 parent.right = child;
@@ -31,31 +40,34 @@ public class TreeCheck {
             }
 
             if (child.parent != null) {
-                return false;
+                return "E3";
             }
             child.parent = parent;
 
             // update ancestor: Union Find like
             if (!updateAncestor(ancestor, relation[0], relation[1])) {
-                return false;
+                return "E3";
             }
 
             nodeMap.put(relation[0], parent);
             nodeMap.put(relation[1], child);
         }
-        return checkOneAncestor(ancestor);
-
+        return checkOneAncestor(ancestor) == true ? "S" : "E4";
 
     }
 
+    /**
+     *
+     * @param ancestor
+     * @param parent
+     * @param child
+     * @return boolean false -> cycle
+     */
     private boolean updateAncestor(int[] ancestor, int parent, int child) {
 
         // find root; set ancestor to root
         if (ancestor[parent] == -1) {
             ancestor[child] = parent;
-        //same ancestor -> child is somehow in the tree
-        } else if (ancestor[parent] == ancestor[child]) {
-            return false;
         } else {
             // update recursively
             updateAncestor(ancestor, ancestor[parent], parent);
